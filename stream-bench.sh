7e8c752db27c3d53ba5648196554e8fcab56e64c
Benjamin Abernathy <ben@babernathy.net>
2018-08-31 09:07:52 -0600

    Added docker compose to pull down test dependencies for local tests. Updated root POM to use profiles when building.

--- stream-bench.sh
+++ stream-bench.sh
@@ -140,35 +140,35 @@ run() {
     $MVN clean install -Dspark.version="$SPARK_VERSION" -Dkafka.version="$KAFKA_VERSION" -Dflink.version="$FLINK_VERSION" -Dstorm.version="$STORM_VERSION" -Dscala.binary.version="$SCALA_BIN_VERSION" -Dscala.version="$SCALA_BIN_VERSION.$SCALA_SUB_VERSION" -Dapex.version="$APEX_VERSION"
 
     #Fetch and build Redis
-    REDIS_FILE="$REDIS_DIR.tar.gz"
-    fetch_untar_file "$REDIS_FILE" "http://download.redis.io/releases/$REDIS_FILE"
+    #REDIS_FILE="$REDIS_DIR.tar.gz"
+    #fetch_untar_file "$REDIS_FILE" "http://download.redis.io/releases/$REDIS_FILE"
 
-    cd $REDIS_DIR
-    $MAKE
-    cd ..
+    #cd $REDIS_DIR
+    #$MAKE
+    #cd ..
 
     #Fetch Apex
-    APEX_FILE="$APEX_DIR.tgz.gz"
-    fetch_untar_file "$APEX_FILE" "$APACHE_MIRROR/apex/apache-apex-core-$APEX_VERSION/apex-$APEX_VERSION-source-release.tar.gz"
-    cd $APEX_DIR
-    $MVN clean install -DskipTests
-    cd ..
+    #APEX_FILE="$APEX_DIR.tgz.gz"
+    #fetch_untar_file "$APEX_FILE" "$APACHE_MIRROR/apex/apache-apex-core-$APEX_VERSION/apex-$APEX_VERSION-source-release.tar.gz"
+    #cd $APEX_DIR
+    #$MVN clean install -DskipTests
+    #cd ..
 
     #Fetch Kafka
-    KAFKA_FILE="$KAFKA_DIR.tgz"
-    fetch_untar_file "$KAFKA_FILE" "$APACHE_MIRROR/kafka/$KAFKA_VERSION/$KAFKA_FILE"
+    #KAFKA_FILE="$KAFKA_DIR.tgz"
+    #fetch_untar_file "$KAFKA_FILE" "$APACHE_MIRROR/kafka/$KAFKA_VERSION/$KAFKA_FILE"
 
     #Fetch Storm
-    STORM_FILE="$STORM_DIR.tar.gz"
-    fetch_untar_file "$STORM_FILE" "$APACHE_MIRROR/storm/$STORM_DIR/$STORM_FILE"
+    #STORM_FILE="$STORM_DIR.tar.gz"
+    #fetch_untar_file "$STORM_FILE" "$APACHE_MIRROR/storm/$STORM_DIR/$STORM_FILE"
 
     #Fetch Flink
-    FLINK_FILE="$FLINK_DIR-bin-hadoop27-scala_${SCALA_BIN_VERSION}.tgz"
-    fetch_untar_file "$FLINK_FILE" "$APACHE_MIRROR/flink/flink-$FLINK_VERSION/$FLINK_FILE"
+    #FLINK_FILE="$FLINK_DIR-bin-hadoop27-scala_${SCALA_BIN_VERSION}.tgz"
+    #fetch_untar_file "$FLINK_FILE" "$APACHE_MIRROR/flink/flink-$FLINK_VERSION/$FLINK_FILE"
 
     #Fetch Spark
-    SPARK_FILE="$SPARK_DIR.tgz"
-    fetch_untar_file "$SPARK_FILE" "$APACHE_MIRROR/spark/spark-$SPARK_VERSION/$SPARK_FILE"
+    #SPARK_FILE="$SPARK_DIR.tgz"
+    #fetch_untar_file "$SPARK_FILE" "$APACHE_MIRROR/spark/spark-$SPARK_VERSION/$SPARK_FILE"
 
   elif [ "START_ZK" = "$OPERATION" ];
   then
@@ -315,9 +315,9 @@ run() {
     run "STOP_ZK"
   elif [ "SPARK_TEST" = "$OPERATION" ];
   then
-    run "START_ZK"
-    run "START_REDIS"
-    run "START_KAFKA"
+    #run "START_ZK"
+    #run "START_REDIS"
+    #run "START_KAFKA"
     run "START_SPARK"
     run "START_SPARK_PROCESSING"
     run "START_LOAD"
